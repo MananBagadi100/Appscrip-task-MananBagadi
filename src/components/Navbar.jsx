8575d4c4239f2849997b1871940bf76add91668c
MananBagadi100 <mananbagadi100@gmail.com>
2025-12-14 11:46:47 +0530

    Added Complete functional navbar as per the figma file for desktop with minor UI changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import './../styles/NavbarStyles.css';
-
+import profileIcon from '../assets/icons/profile.svg'
+import brandIcon from './../assets/icons/Logo.svg'
+import chevronDown from '../assets/icons/chevronDown.svg'
 import menuIcon from '../assets/icons/hamburgerBtn.svg';
 import searchIcon from '../assets/icons/search-icon.svg';
 import heartIcon from '../assets/icons/heart.svg';
@@ -7,21 +9,39 @@ import bagIcon from '../assets/icons/shopping-bag.svg';
 
 function Navbar() {
     return (
-        <header className="navbar">
-            <div className="navbar__left">
-                <img src={menuIcon} alt="Menu" />
-            </div>
+        <>
+            <header className="navbar">
+                <div className="navbar__left">
+                    <img src={menuIcon} alt="Menu" className="navbar__mobile-only" />
+                    <img src={brandIcon} alt="Brand" className="navbar__desktop-only" style={{width:36,height:36}}/>
+                </div>
 
-            <div className="navbar__center">
-                <span className="navbar__logo">LOGO</span>
-            </div>
+                <div className="navbar__center">
+                    <span className="navbar__logo">LOGO</span>
+                </div>
 
-            <div className="navbar__right">
-                <img src={searchIcon} alt="Search" />
-                <img src={heartIcon} alt="Wishlist" />
-                <img src={bagIcon} alt="Cart" />
-            </div>
-        </header>
+                <div className="navbar__right">
+                    <img src={searchIcon} alt="Search" />
+                    <img src={heartIcon} alt="Wishlist" />
+                    <img src={bagIcon} alt="Cart" />
+
+                    {/* Icons for Desktop-only */}
+                    <img src={profileIcon} alt="Profile" className="navbar__desktop-only" />
+                    <div className="navbar__desktop-only navbar__language">
+                        ENG <img src={chevronDown} alt="Language" />
+                    </div>
+                </div>
+            </header>
+
+            {/* Navbar for Desktop only */}
+            <nav className="navbar__desktop-links">
+                <span>SHOP</span>
+                <span>SKILLS</span>
+                <span>STORIES</span>
+                <span>ABOUT</span>
+                <span>CONTACT US</span>
+            </nav>
+        </>
     );
 }
 
