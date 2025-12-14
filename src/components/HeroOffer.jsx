283a1982ba8b0bb4fb1c3115b05c2f416feaee2f
MananBagadi100 <mananbagadi100@gmail.com>
2025-12-14 10:29:09 +0530

    Hero Offer Section Completed with responsiveness for desktop

--- src/components/HeroOffer.jsx
+++ src/components/HeroOffer.jsx
@@ -1,15 +1,33 @@
 import PhoneNavigation from './../assets/icons/TopbarMobile.svg'
+import HeroOfferContentLogo from './../assets/icons/HeroOfferContentLogo.svg'
 import './../styles/HeroOfferStyles.css'
+
 function HeroOffer() {
-    return (
-        <section
-            className='Phone-system-navigation-bar-section'
-        >
-            <img src={PhoneNavigation} alt="Phone Navigation" />
-            <div>
-                Lorem ipsum dolor
-            </div>
-        </section>
-    )
+  return (
+    <>
+      {/* MOBILE HERO (already working) */}
+      <section className="Phone-system-navigation-bar-section">
+        <img src={PhoneNavigation} alt="Phone Navigation" />
+        <div>Lorem ipsum dolor</div>
+      </section>
+
+      {/* DESKTOP HERO */}
+      <section className="Desktop-system-navigation-bar">
+        <div className="desktop-hero-item">
+          <img src={HeroOfferContentLogo} alt='OfferLogo' className="desktop-hero-icon"/>
+          <span>Lorem ipsum dolor</span>
+        </div>
+        <div className="desktop-hero-item">
+          <img src={HeroOfferContentLogo} alt='OfferLogo' className="desktop-hero-icon"/>
+          <span>Lorem ipsum dolor</span>
+        </div>
+        <div className="desktop-hero-item">
+          <img src={HeroOfferContentLogo} alt='OfferLogo' className="desktop-hero-icon"/>
+          <span>Lorem ipsum dolor</span>
+        </div>
+      </section>
+    </>
+  )
 }
+
 export default HeroOffer
